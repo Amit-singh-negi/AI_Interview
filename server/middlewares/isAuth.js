2c8cf27e46a75b4ba49644bbefb833d31d707c70
amitneg <asnegi885@gmail.com>
2026-08-14 10:49:25 +0530

    isAuth.js

---
+++ server/middlewares/isAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken"
+
+const isAuth = async (req,res,next) =>{
+     console.log("Cookies:", req.cookies);
+    try {
+        let {token} = req.cookies
+
+        if(!token){
+        return res.status(400).json({message:"user does not have a token"})
+        }
+        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
+
+        if(!verifyToken){
+            return res.status(400).json({message:"user does not have a valid token"})
+        }
+        req.userId = verifyToken.userId
+
+        next()
+        
+    } catch (error) {
+        return res.status(400).json({message:`isAuth error ${error}`})
+    }
+}
+
+export default isAuth;
