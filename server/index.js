3d138c327cb8e5b471d8e7053ccad652f3331a0b
amitneg <asnegi885@gmail.com>
2026-08-18 10:57:05 +0530

    index.js

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDb from './config/connectDb.js';
+dotenv.config()
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+import authRouter from './routes/auth.route.js'
+import userRoute from './routes/userRoute.js';
+import interviewRouter from './routes/interviewRoute.js';
+import paymentRouter from './routes/payment.route.js';
+
+const app = express();
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRoute)
+app.use("/api/interview", interviewRouter)
+app.use("/api/payment",paymentRouter)
+
+const PORT = process.env.PORT || 6000
+
+
+
+ app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+    connectDb()
+    
+ })
+
+
+ 
