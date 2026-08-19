7f3ca0c04aa51700528fc0eaf07db5ae24883266
Amit Singh Negi <asnegi885@gmail.com>
2026-08-19 12:07:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from './routes/payment.route.js';
 
 const app = express();
 app.use(cors({
-    origin:"https://ai-interview-client-z547.onrender.com",
+    origin:"https://ai-interview-client-afiy.onrender.com",
     credentials:true
 }))
 
